09c872f3b7add015ca3c221d9a6ae63b53a92953
Jaroslaw Majcherczyk <jaroslaw.majcherczyk@gmail.com>
2023-10-27 16:47:31 +0200

    last commit for now, added react-toastify, formatBackground and lasts props

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import TempAndDetails from "./components/TempAndDetails"
 import TimeLocation from "./components/TimeLocation"
 import TopButton from "./components/TopButton"
 import getFormattedWeatherData from "./services/weatherService"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
 
@@ -16,7 +19,14 @@ function App() {
 useEffect(() => {
   const fatchWeather = async () =>
   {
+    const message = query.q ? query.q : "poprawnej lokalizacji.";
+
+    toast.info("Pobierasz pogode z " + message);
+
  await getFormattedWeatherData({...query, units}).then((data) =>{
+
+  toast.success(`Prawidłowe pobranie pogody z ${data.name} , ${data.country}`)
+
   setWeather(data);
   
  })
@@ -26,13 +36,21 @@ useEffect(() => {
   
 }, [query, units]);
 
+const formatBackground = () => {
+  if(!weather) return 'from-cyan-700 to-blue-800'
+  const threshold = units === "metric" ? 20 : 60
+  if (weather.temp <= threshold) return 'from-cyan-700 to-blue-800'
+
+  return "from-yellow-600 to-orange-600"
+}
 
 console.log(weather)
   return (
     <>
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-800 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButton setQuery/>
-      <Inputs setQuery units setUnits/>
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-800 to-blue-700 h-fit shadow-xl shadow-gray-400 
+    ${formatBackground()}`}>
+      <TopButton setQuery={setQuery}/>
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
 
         {weather && (
           <div> 
@@ -42,6 +60,9 @@ console.log(weather)
         <Forecust title={"Pogoda na najbliższe dni"} items={weather.daily}/>
           </div>
           )}
+
+    <ToastContainer autoClose={2000} theme="colored"  newestOnTop={true}/>
+
     </div>
    
     </>
--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -1,30 +1,34 @@
 
 
-const TopButton = () => {
+const TopButton = ({setQuery}) => {
 
     const cities = [
         {
             id: 0,
-            title:"london"
+            title:"Londyn"
         },
         {
             id: 1,
-            title:"nederland"
+            title:"Amsterdam"
         },
         {
             id: 2,
-            title:"poland"
+            title:"Warszawa"
         },
         {
             id: 3,
-            title:"germany"
+            title:"Berlin"
+        },
+        {
+            id: 4,
+            title:"Paryż"
         },
     ]
 
   return (
     <div className='flex items-center justify-around my-6'>
             {cities.map((city) => (
-                <button key={city.id} className='text-white font-medium text-lg'>
+                <button key={city.id} onClick={() => setQuery({q: city.title})} className='text-white font-medium text-lg'>
                     {city.title}
                 </button>
             ))}
